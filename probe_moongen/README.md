9e07062890f168ecc0dd5ed403e15be61b3fe4ca
Christos Sakkas <chsakkas@hotmail.com>
2018-07-26 17:21:52 +0300

    readmes

--- probe_moongen/README.md
+++ probe_moongen/README.md
@@ -1,21 +1,16 @@
-# tng-moongen-probe
+# MoonGen probe
 
 <p align="center"><img src="https://github.com/sonata-nfv/tng-api-gtw/wiki/images/sonata-5gtango-logo-500px.png" /></p>
 
-5GTANGO VnV Platform Moongen probe
-
-[Media Net Lab - NCSR Demokritos](http://www.medianetlab.gr/)
-
-This is a network testing sytem using MoonGen packet generator.
 
 # Description
 
-  - moongen-agent-v2.py is used to receive the start test command with test parameters and to send the results back to the the entity that started the test.
+  - moongen-agent-v3.py is used to receive the start test command with test parameters and to send the results back to the the entity that started the test.
   - moongentest.lua is the test the actual test that will be run on the system.
 
 #### System Input
 
-The message format of the UDP packet received by moongen-agent-v2.py is -> NS input endpoint IP:NS output endpoint IP:Test bandwidth:Test execution time:Test ID:MoonGen outbound traffic port:Moongen inbound traffic port
+The message format of the UDP packet received by moongen-agent-v3.py is -> NS input endpoint IP:NS output endpoint IP:Test bandwidth:Test execution time:Test ID:MoonGen outbound traffic port:Moongen inbound traffic port
 
 Examples
 
@@ -33,22 +28,17 @@ A JSON file is produced containing all the measurements of the test. This file i
 
 # Installation
 
-moongen-agent-v2.py and moongentest.lua need to be in the folder of MoonGen. moongen-agent-v2.py needs to be edited in order to have correct IP and PORT depending on the configuration of your network.
+moongen-agent-v3.py and moongentest.lua need to be in the folder of MoonGen. moongen-agent-v3.py needs to be edited in order to have correct IP and PORT depending on the configuration of your network.
 
-To run moongen-agent-v2.py in the background use:
+To run moongen-agent-v3.py in the background use:
 
 ```sh
-$ nohup python moongen-agent-v2.py &
+$ nohup python moongen-agent-v3.py &
 ```
 
-# Todos
-
- - Many iterations of the same test as parameter
- - Allow multiple traffic flows at the same time
- - Add more tests
 
 License
 ----
 
-tng-moongen-probe is published under Apache 2.0 license. Please see the LICENSE file for more details.
+tng-probes is published under Apache 2.0 license. Please see the LICENSE file for more details.
 
--- probe_moongen/moongen-agent-v2.py
+++ probe_moongen/moongen-agent-v2.py
@@ -6,8 +6,8 @@ import scpclient
 import json
 
 
-UDP_IP = "10.10.1.15"
-UDP_PORT = 33339
+UDP_IP = "10.30.0.253"
+UDP_PORT = 33333
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
@@ -15,13 +15,27 @@ sock.bind((UDP_IP, UDP_PORT))
 
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-        data, addr = sock.recvfrom(2048) # buffer size is 1024 bytes
-        data2 = data.split(':')
-        print data2
-        time.sleep(1)
+
+
+def runTest(var0, var1,var2,var3,var4,var5):
+
+        if (var5=="64" or var5=="128" or var5=="256" or var5=="512" or var5=="1024" or var5=="1280" or var5=="1518"):
+            var0 = var0 + "-" + var5
+
+
+        print "================================"
+        print var0
+        print var1
+        print var2
+        print var3
+        print var4
+        print var5
+        print "================================"
+
         #os.system("./moongen-simple start chris:2:3:rate="+data2[2]+"mbit/s,timeLimit="+data2[3])
-        os.system("./build/MoonGen moongentest.lua "+data2[5]+" "+data2[6]+" "+data2[3]+" "+data2[2])
+        var5value = int(var5)-18
+        print("./build/MoonGen examples/kristo.lua "+var1+" "+var2+" "+var3+" "+var4 +" -s " + str(var5value))
+        os.system("./build/MoonGen examples/kristo.lua "+var1+" "+var2+" "+var3+" "+var4+" -s " + str(var5value))
         time.sleep(2)
 
         arrayTX=''
@@ -48,6 +62,9 @@ while True:
 
 
         latency = ''
+
+        os.system('python shorten.py 35 histogram.csv')
+
         with open('histogram.csv') as f:
             for line in f:
                 linehelper = line.split(",")
@@ -56,33 +73,57 @@ while True:
         latency = latency[:-1]
 
 
-        resultsJSON = '{"TestID": "'+data2[4]+'","Timestamp": "'+str(time.time())+'","TestConfig": {"Time": '+data2[3]+',"Mbit": '+data2[2]+',"PktSize": 1300,"IpSrc": "10.0.0.10","IpDst": "10.1.0.10","PortSrc": 1234,"PortDst": 319},"Results": {"PktStatsTx": ['+arrayTX+'],"PktStatsRx": ['+arrayRX+'],"LatencyStats": ['+latency+']}}'
+        resultsJSON = '{"TestID": "'+var0+'","Timestamp": "'+str(time.time())+'","TestConfig": {"Time": '+var3+',"Mbit": '+var4+',"PktSize": '+var5+',"IpSrc": "10.0.0.10","IpDst": "10.1.0.10","PortSrc": 1234,"PortDst": 319},"Results": {"PktStatsTx": ['+arrayTX+'],"PktStatsRx": ['+arrayRX+'],"LatencyStats": ['+latency+']}}'
 
 
 
         d = json.loads(resultsJSON)
 
-        DataFile = open(data2[4]+".json", "w")
+        DataFile = open(var0+".json", "w")
         DataFile.write(json.dumps(d, indent=4, sort_keys=True))
         DataFile.close()
 
         time.sleep(2)
         print "sending json to orch"
-        ssh_client = paramiko.SSHClient()
-        ssh_client.load_system_host_keys()
-        ssh_client.connect('public_server_ip/hostname', username='myusername', password='mypassword')
+        #ssh_client = paramiko.SSHClient()
+        #ssh_client.load_system_host_keys()
+        #ssh_client.connect('aias.iit.demokritos.gr', username='xilouris', password='<czx1.4>')
 
-        with scpclient.closing(scpclient.Write(ssh_client.get_transport(), '.')) as scp:
-            scp.send_file(data2[4]+'.json', remote_filename=data2[4]+'.json')
-        time.sleep(2)
-        sock2.sendto(data2[4]+".json",("public_server_ip/hostname", 33339))
+        #with scpclient.closing(scpclient.Write(ssh_client.get_transport(), '.')) as scp:
+        #    scp.send_file(data2[4]+'.json', remote_filename=data2[4]+'.json')
+        #time.sleep(2)
+        #sock2.sendto(data2[4]+".json",("aias.iit.demokritos.gr", 33339))
+        os.system('curl -X POST http://pre-int-sp-ath.5gtango.eu:8000/api/v1/active/service/1235/test/'+var0+'/data -d @'+var0+'.json --header "Content-Type: application/json"')
+        os.system('cp '+var0+'.json /var/www/html/latest.json')
+        print 'cp '+var0+'.json /var/www/html/latest.json'
+
+        os.system('python /root/plot-bar-csv.py')
         print "send done"
 
 
 
 
+while True:
+    data, addr = sock.recvfrom(2048) # buffer size is 1024 bytes
+    print data
+    data2 = data.split(':')
+    print data2
+    print "testType -> "+data2[7]
+    time.sleep(1)
+    if (data2[7]=="simple"):
 
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "1300")
 
+    elif (data2[7]=="rfc"):
+        print "rfc testing"
 
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "64")
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "128")
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "256")
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "512")
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "1024")
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "1280")
+        runTest(data2[4], data2[5], data2[6][0], data2[3], data2[2], "1518")
 
-
+    else:
+        print "default"
